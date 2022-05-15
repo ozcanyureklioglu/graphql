d53e4a5dbd42b3c846017e6baf663f4f0ef4316f
Özcan Yüreklioğlu <ozcan.yu@hotmail.com>
2022-05-15 22:55:38 +0300

    user alanınına avatar eklendi ve postlara açıklama eklendi

--- server/src/data2.js
+++ server/src/data2.js
@@ -2,12 +2,14 @@ const users = [
     {
       id: "1",
       fullName: "Özcan Yüreklioğlu",
-      age: 26
+      age: 26,
+      profile_photo:"https://randomuser.me/api/portraits/men/21.jpg"
     },
     {
       id: "2",
-      fullName: "Ahmet Günal",
-      ag:35
+      fullName: "Jessy Morica",
+      ag:35,
+      profile_photo: "https://randomuser.me/api/portraits/women/66.jpg"
     },
   ];
   
@@ -16,16 +18,19 @@ const users = [
       id: "1",
       title: "Özcan gönderisi",
       user_id: "1",
+      description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
     },
     {
       id: "2",
       title: "Özcan diğer gönderisi",
       user_id: "1",
+      description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
     },
     {
       id: "3",
-      title: "Ahmet'in gönderisi",
+      title: "Jessy'in gönderisi",
       user_id: "2",
+      description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
     },
   ];
   
